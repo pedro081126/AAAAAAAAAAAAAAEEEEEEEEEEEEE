466052487d49d8dc8c0f553d1aba0ae8d55c2240
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-03-13 08:36:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ const botoes = document.querySelectorAll (".botao");
 for(let i=0;i<botoes.lenght;i++){
 botoes[i].onclick=function(){
 
+  for(let j=0;j<botoes.lenght;j++){
+    botoes.classlist.remove("ativo);
+    }
+
+  botoes[i].classlist.add("ativo");
+}
+}
+
 
