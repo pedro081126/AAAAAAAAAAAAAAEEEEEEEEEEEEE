dcf8991c50cccec9c93bdb6914b526273eae5b2b
pedro081126 <128717103+pedro081126@users.noreply.github.com>
2024-03-25 09:25:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.romove("ativo);
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo);
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-03-25T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1-tempoAtual;
+ 
